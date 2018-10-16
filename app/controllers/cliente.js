97fa3ab1b3a951e93ba74dca8e5c9480409c4453
Jonathan <jontopanotti@gmail.com>
2018-10-16 20:24:44 -0300

    Testing async

--- app/controllers/cliente.js
+++ app/controllers/cliente.js
@@ -4,17 +4,15 @@ const pool = new Pool({
   ssl: true
 });
 
-module.exports = function(){
-    var listUsuarios = async (req, res) => {
-       try {
-         const client = await pool.connect();
-         const result = await client.query('SELECT * FROM usuarios');
-         const results = {'results ': (result) ? result.rows : null};
-         res.send(JSON.stringify(results));
-         client.release();
-       } catch (err){
-         console.error(err);
-         res.send("Error "  + err);
-       }
-     };
-}
+module.exports.listUsuarios = async (req, res) => {
+  try {
+     const client = await pool.connect();
+     const result = await client.query('SELECT * FROM usuarios');
+     const results = {'results ': (result) ? result.rows : null};
+     res.send(JSON.stringify(results));
+     client.release();
+  } catch (err){
+     console.error(err);
+     res.send("Error "  + err);
+  }
+};
