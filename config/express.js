4ae716f015a2328c2e456424273ad24e86dbcf87
JTopanotti <jontopanotti@gmail.com>
2018-10-15 23:11:09 -0300

    Postgres Config Testing

--- config/express.js
+++ config/express.js
@@ -2,8 +2,26 @@ var express = require('express');
 //var load = require('express-load');
 //var bodyParser = require('body-parser');
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 module.exports = function(){
   var app = express();
+  app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = {'results': (result) ? result.rows : null};
+      res.render(results);
+      client.release();
+    } catch (err){
+      console.error(err);
+      res.send("Error "  + err);
+    }
+  });
   //var home = require('../app/routes/home');
 
   app.set('port', 3001);
@@ -22,4 +40,3 @@ module.exports = function(){
 
   return app;
 };
-
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 var app = require('./config/express')();
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express Server ' + app.get('port');
+  console.log('Express Server ' + app.get('port'));
 });
