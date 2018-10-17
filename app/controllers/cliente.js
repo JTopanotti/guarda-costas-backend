7c5d8edca353e35043df69ea5e780d47c50a130f
JTopanotti <jontopanotti@gmail.com>
2018-10-17 11:41:13 -0300

    Adding Header

--- app/controllers/cliente.js
+++ app/controllers/cliente.js
@@ -8,9 +8,9 @@ module.exports.listUsuarios = async (req, res) => {
   try {
      const client = await pool.connect();
      const result = await client.query('SELECT * FROM usuarios');
-     const results = {'results ': (result) ? result.rows : null};
+     const results = {'results': (result) ? result.rows : null};
      client.release();
-     res.status(200).json({'results':results});
+     res.status(200).json(results);
   } catch (err){
      console.error(err);
      res.send("Error "  + err);
