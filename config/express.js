2de96b40e42b4a6462008d89def1435a509844c3
JTopanotti <jontopanotti@gmail.com>
2018-10-17 11:10:51 -0300

    Adding Header

--- config/express.js
+++ config/express.js
@@ -9,7 +9,7 @@ module.exports = function(){
   var cliente = require("../app/routes/cliente");
 
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
