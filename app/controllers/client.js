8f2f12248a6627f68137287638b2cf104d0f3d7a
JTopanotti <jontopanotti@gmail.com>
2018-11-07 17:50:10 -0200

    Testing Sequelize

--- app/controllers/client.js
+++ app/controllers/client.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
 const Sequelize = require('sequelize');
+var Usuarios = require('../models/usuarios');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -13,13 +14,14 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 
 module.exports.listClients = async (req, res) => {
 
-  console.log("Got here");
+  console.log(Usuarios);
+  res.status(200);
 
-  sequelize.authenticate().then(() => {
-    console.log("Sequelize auth successful");
-  }).catch(() => {
-    console.log("Sequelize auth error");
-  })
+  // sequelize.authenticate().then(() => {
+  //   console.log("Sequelize auth successful");
+  // }).catch(() => {
+  //   console.log("Sequelize auth error");
+  // })
 
   // try {
   //    const client = await pool.connect();
--- app/models/usuarios.js
+++ app/models/usuarios.js
@@ -5,7 +5,43 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 
 
 var client = sequelize.define('usuarios', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: Sequelize.STRING(45),
+    unique: "usuarios_username_un",
+    defaultValue: null
+  },
+  password: {
+    type: Sequelize.STRING(45),
+    allowNull: false,
+    defaultValue: "pass123"
+  },
+  data_hora_cadastro: {
+    type: Sequelize.DATE,
+    defaultValue: null
+  },
+  email: {
+    type: Sequelize.STRING(45),
+    unique: "usuarios_email_un",
+    allowNull: false
+  },
+  data_hora_nascimento: {
+    type: Sequelize.DATE,
+    defaultValue: null
+  },
+  telefone: {
+    type: Sequelize.STRING(11),
+    defaultValue: null
+  },
+  nome: {
+    type: Sequelize.STRING(11),
+    allowNull: false
+  }
 
-});
+}, {timestamps: false});
 
 module.exports = client;
